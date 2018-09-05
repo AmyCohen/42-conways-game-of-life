f88d93c77356cf35b26518dbad5392cef75e2675
Amy Cohen <anbcohen@gmail.com>
2018-09-05 07:01:07 -0700

    added comments on java docs for drawing graphics and wrote out the helper methods that we discussed in class

--- app/src/main/java/com/amycohen/lab42conwaygameoflife/Square.java
+++ app/src/main/java/com/amycohen/lab42conwaygameoflife/Square.java
@@ -1,7 +1,17 @@
 package com.amycohen.lab42conwaygameoflife;
 
+/*
+Java Docs on drawing graphics:
+
+https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html#drawRect(int,%20int,%20int,%20int)
+
+drawRect(int x, int y, int width, int height)
+ */
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 public class Square {
-    //drawRect(int x, int y, int width, int height)
     private float xx;
     private float yy;
     private float width;
@@ -15,6 +25,29 @@ public class Square {
         this.height = height;
         this.color = color;
     }
+
+    public void setX(float xx) {
+        this.xx = xx;
+    }
+
+    public void setY(float yy) {
+        this.yy = yy;
+    }
+
+    public float getX() {
+        return this.xx;
+    }
+
+    public float getY() {
+        return this.yy;
+    }
+
+    public void draw(Canvas canvas) {
+        Paint brush = new Paint(Paint.ANTI_ALIAS_FLAG);
+        brush.setColor(this.color);
+        canvas.drawRect(this.xx, this.yy, this.width, this.height, brush);
+    }
+
 
 
 }
